c49952d579735aca3cad1e54b0c4e90f215c948c
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-10-11 15:29:07 +0700

    Update github actions to run tests. Add failing tests to make sure that the new github action actually catches them.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey_validation(t *testing.T) {
+	tests := []struct {
+		name        string
+		headerKey   string
+		headerValue string
+		want        string
+	}{
+		{"no auth header", "Poodonkis", "Bearer 00000", ErrNoAuthHeaderIncluded.Error()},
+		// {"malformed auth header", "Authorization", "Bearer 00000", "malformed authorization header"},
+		{"malformed auth header", "Authorization", "ApiKey 00000", "malformed authorization header"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reqHeader := http.Header{}
+			reqHeader.Add(tt.headerKey, tt.headerValue)
+
+			_, err := GetAPIKey(reqHeader)
+			if err != nil {
+				if err.Error() != tt.want {
+					t.Errorf("GetAPIKey() err == %s; want == %s", err.Error(), tt.want)
+				}
+			} else {
+				t.Error("Expected an error")
+			}
+		})
+	}
+}
+
+func TestGetAPIKey_checkWorking(t *testing.T) {
+	tests := []struct {
+		name        string
+		headerKey   string
+		headerValue string
+		want        string
+	}{
+		// {"valid header with key", "Authorization", "ApiKey 00000", "00000"},
+		{"valid header with key", "Authorization", "ApiKey 11111", "00000"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reqHeader := http.Header{}
+			reqHeader.Add(tt.headerKey, tt.headerValue)
+
+			apiKey, err := GetAPIKey(reqHeader)
+			if err != nil {
+				t.Fatalf("GetAPIKey() error: %v", err)
+			}
+
+			if apiKey != tt.want {
+				t.Errorf("got %q; want %q", apiKey, tt.want)
+			}
+		})
+	}
+}
